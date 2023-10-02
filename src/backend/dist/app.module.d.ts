5f3f399f0645f8c8206907b102b782a34ff35d38
Shreekrishna Ravishankara Bhat <sbhat97@gatech.edu>
2023-10-02 18:10:38 -0400

    Removed the backend folder since it's in its own repo

--- src/backend/dist/app.module.d.ts
+++
@@ -1,7 +0,0 @@
-import { FirebaseService } from './firebase/firebase.service';
-import { yahooFinanceService } from './yahooFinance/yahooFinance.service';
-export declare class AppModule {
-    private firebaseService;
-    private yahooFinanceService;
-    constructor(firebaseService: FirebaseService, yahooFinanceService: yahooFinanceService);
-}
--- src/backend/src/yahooFinance/yahooFinance.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { yahooFinanceController } from './yahooFinance.controller';
-import { yahooFinanceService } from './yahooFinance.service';
-
-@Module({
-  controllers: [yahooFinanceController],
-  providers: [yahooFinanceService],
-})
-export class yahooFinanceModule {}
